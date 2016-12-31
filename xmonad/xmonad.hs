f560d43ad83ef87d707409e20111158897bf43e4
James Russo <okabeR@users.noreply.github.com>
2016-12-31 13:02:45 -0500

    redo

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,69 +1,56 @@
 import XMonad
+import qualified Data.Map as M
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
-import XMonad.Util.Run(spawnPipe)
-import XMonad.Util.EZConfig(additionalKeys)
-import System.IO
 import XMonad.Layout.ResizableTile
-import Control.Monad (liftM2)
-import Data.List 
-import qualified XMonad.StackSet as W
-import XMonad.Hooks.ManageHelpers
-import XMonad.Actions.CycleWS
-import XMonad.Layout.ToggleLayouts
-import XMonad.Actions.FloatKeys
 import XMonad.Layout.Spacing
-import XMonad.Layout.Renamed
-import XMonad.Layout.Grid
-import XMonad.Layout.ResizeScreen
-import XMonad.Layout.SimplestFloat
-import XMonad.Hooks.Place
+import XMonad.Util.EZConfig
+import qualified XMonad.StackSet as W
+import XMonad.Layout.PerWorkspace
+import Control.Monad (liftM2)
 
 myManageHook = composeAll
-   [ className =? "mpv"  --> doFloat 
-   , className =? "mpv" --> viewShift "5" 
-   , isFullscreen --> doFullFloat 
-   , className =? "panel" -->  doIgnore 
-   , className =? "trayer" --> doIgnore
-   , manageDocks 
-   , placeHook (inBounds (simpleSmart))
-   ]
-   where viewShift = doF . liftM2 (.) W.greedyView W.shift 
-  
+    [ className =? "mpv"      --> doFloat
+    , className =? "MuPDF" --> viewShift "5"
+    , className =? "Lxappearance" --> doFloat
+    , className =? "Geany" --> doShift "3"
+    ]
+    where viewShift = doF . liftM2 (.) W.greedyView W.shift
 
-main = do
-     xmproc <- spawnPipe "xmobar"
-     xmonad $ defaultConfig
-        { manageHook = myManageHook <+> manageHook defaultConfig
-        , layoutHook = avoidStruts  ( myLayout ||| mirrorLayout ||| gridLayout ||| Grid ||| simplestFloat ||| layoutHook defaultConfig)  
-        ,handleEventHook = mconcat
+
+main = xmonad =<< xmobar myConfig
+
+myConfig = defaultConfig
+    { terminal    = "urxvt"
+    , modMask     = mod4Mask
+    , borderWidth = 3
+    , keys          = \c -> mykeys c `M.union` keys defaultConfig c
+    , workspaces = myWorkspaces
+    , logHook = dynamicLog
+    , manageHook = manageDocks <+> myManageHook <+> manageHook defaultConfig
+    , layoutHook = avoidStruts  $  
+                   onWorkspace "1" firstL $
+                   onWorkspace "3" numTwo $
+                   myLayout
+    , handleEventHook = mconcat
                          [ docksEventHook
-                         , handleEventHook defaultConfig ]
-	,logHook = dynamicLogWithPP xmobarPP
-                        { ppOutput = hPutStrLn xmproc
-                        , ppTitle = xmobarColor "#9EEE81" "" . shorten 50
-                        }        
-        , modMask = mod4Mask
-	,terminal = "urxvt"
-	,borderWidth = 3
-        }`additionalKeys`
- 	[  ((mod4Mask, xK_x), kill)
-        , ((mod4Mask , xK_p), spawn "dmenu_extended_run")  
-	, ((mod4Mask,               xK_a), sendMessage MirrorShrink)
- 	, ((mod4Mask,               xK_z), sendMessage MirrorExpand)
-        , ((mod4Mask,               xK_c), toggleWS)
-        , ((mod4Mask, 		    xK_f), sendMessage (Toggle "Full"))  
-	, ((mod4Mask, 		    xK_s), withFocused $ float)
-        , ((mod4Mask,               xK_Down  ), withFocused (keysResizeWindow (-10,-10) (0,0)))
- 	, ((mod4Mask,               xK_Up    ), withFocused (keysResizeWindow (20,20) (0,0)))
-  	, ((mod4Mask .|. shiftMask, xK_Left  ), withFocused (keysMoveWindow (-10,0) ))
- 	, ((mod4Mask .|. shiftMask, xK_Right    ), withFocused (keysMoveWindow (10,0) ))
-	, ((mod4Mask .|. shiftMask, xK_Up  ), withFocused (keysMoveWindow (0,-10) ))
- 	, ((mod4Mask .|. shiftMask, xK_Down    ), withFocused (keysMoveWindow (0,10) ))
- 	, ((mod4Mask, xK_w), placeFocused (withGaps (16,0,16,0) (simpleSmart)) ) 	 
-	]
-      
-myLayout =  toggleLayouts Full $ renamed [Replace "fsociety"] $ goodLayout    
-goodLayout = spacing 8 $ ResizableTall 1 (3/100) (1/2) []
-gridLayout =  renamed [Replace "Rice"] $ spacing 5 $ resizeHorizontalRight 900 (Mirror $ Tall 1 (3/100) (1/2))  
-mirrorLayout = spacing 8 $ Mirror ( ResizableTall 1 (3/100) (1/2) [] )
+                         , handleEventHook defaultConfig ]   
+    }
+   where
+    mykeys (XConfig {modMask = modm}) = M.fromList $
+         [ ((modm , xK_x), kill) 
+         , ((modm, xK_p), spawn "dmenu_run")
+         , ((modm,               xK_a), sendMessage MirrorShrink)
+         , ((modm,               xK_z), sendMessage MirrorExpand)
+	 ]
+         ++
+         [((m .|. mod4Mask, k), windows $ f i) -- Replace 'mod1Mask' with your mod key of choice.
+         | (i, k) <- zip myWorkspaces [xK_1 .. xK_9]
+         , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
+
+myWorkspaces = ["1","2","3","4","5","6","7","8","9"]
+
+
+myLayout =  (spacing 10 $ ResizableTall 1 (3/100) (1/2) []) ||| ( spacing 10 $ (Mirror $ ResizableTall 1 (3/100) (1/2) [])) ||| layoutHook defaultConfig
+firstL = spacing 15 $ ResizableTall 1 (3/100) (1/2) []
+numTwo = spacing 20 $ ResizableTall 1 (3/100) (1/2) []
